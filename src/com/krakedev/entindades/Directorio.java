3fc08cdc6c367c644d7995ed287b2745bff0e2e4
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-22 16:28:54 -0400

    feat(directorio): add attributes, getters and setters

---
+++ src/com/krakedev/entindades/Directorio.java
@@ -0,0 +1,17 @@
+package com.krakedev.entindades;
+
+import java.util.ArrayList;
+
+public class Directorio {
+	
+	ArrayList<Contacto> contactos;
+
+	public ArrayList<Contacto> getContactos() {
+		return contactos;
+	}
+
+	public void setContactos(ArrayList<Contacto> contactos) {
+		this.contactos = contactos;
+	}
+	
+}
